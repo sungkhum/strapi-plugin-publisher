56605b17e7e92859d06584c59d9ea22ffa9ebde7
rtibaldo <116676475+rtibaldo@users.noreply.github.com>
2025-03-26 17:05:09 +0100

    fix: resolve issue with model config namespace notation (#169)

--- server/services/settings-service.js
+++ server/services/settings-service.js
@@ -2,6 +2,6 @@ import pluginId from '../utils/pluginId';
 
 export default ({ strapi }) => ({
 	get() {
-		return strapi.config.get(`plugin.${pluginId}`);
+		return strapi.config.get(`plugin::${pluginId}`);
 	}
 });
