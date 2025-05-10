a059eeb28efa3b3bdcbfcc453aa7b03abdb05d7d
sungkhum <sungkhum@gmail.com>
2025-05-10 10:41:50 -0700

    Update schema.js

--- server/config/schema.js
+++ server/config/schema.js
@@ -5,7 +5,7 @@ const pluginConfigSchema = yup.object().shape({
 		.object()
 		.shape({
 			syncFrequency: yup.string().optional(),
-			defaultTimezone: yup.string().nullable(),
+			defaultTimezone: yup.string().optional(),
 		})
 		.optional(),
 	hooks: yup.object().optional(),
